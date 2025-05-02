04545c9af2ef4ed6e8408fb0ce0de2e3d97df812
Innocent <99240481+Asabuodu@users.noreply.github.com>
2025-05-02 15:00:46 +0100

    currence conerter working

--- app/components/CurrencyConverter.tsx
+++ app/components/CurrencyConverter.tsx
@@ -1,5 +1,11 @@
+"use client";
 import { useState } from 'react';
 import axios from 'axios';
+
+interface ConversionResponse {
+  result: number;
+  rate: number;
+}
 
 const CurrencyConverter = () => {
   const [amount, setAmount] = useState('');
@@ -9,20 +15,23 @@ const CurrencyConverter = () => {
   const [rate, setRate] = useState<number | null>(null);
   const [loading, setLoading] = useState(false);
 
-  const currencies = ['USD', 'EUR', 'GBP', 'JPY', 'CAD', 'AUD', 'INR', 'SGD'];
+  const currencies = ['USD', 'EUR', 'GBP', 'JPY', 'CAD', 'AUD', 'INR', 'SGD', 'CNY', 'CHF','BRL', 'RUB', 'ZAR', 'MXN', 'NZD', 'HKD', 'SEK', 'NOK', 'DKK', 'PLN', 'THB ','MYR', 'IDR', 'PHP', 'TRY', 'CZK', 'HUF', 'ILS', 'AED', 'SAR', 'QAR', 'KWD', 'BHD', 'OMR ', 'JOD', 'TWD', 'VND', 'PKR', 'CLP', 'COP', 'PEN', 'ARS', 'UYU', 'VEF', 'NIO', 'GTQ', 'CRC', 'DOP', 'HNL', 'SVC', 'PYG', 'BAM', 'MKD', 'ALL', 'MDL', 'UAH', 'BYN', 'KZT ', 'AZN', 'GEL', 'AMD', 'NOK', 'ISK', 'LTL', 'LVL', 'EUR', 'XOF', 'XAF', 'XPF', 'XDR', 'XAU', 'XAG ', 'XPT', 'XPD', 'BTC', 'ETH', 'LTC', 'XMR', 'DASH', 'ZEC', 'DOGE', 'BNB', 'SOL', 'ADA', 'DOT', 'TRX', 'MATIC', 'AVAX', 'LINK', 'XLM', 'ALGO', 'ATOM', 'VET', 'FIL', 'ICP ', 'MANA', 'SAND', 'AAVE', 'COMP', 'CRV', 'YFI', 'SNX', 'BAL', 'LDO', 'RUNE', 'KSM', 'DOT', 'FIL', 'ETC', 'ZIL', 'BAT', 'REN', '1INCH', 'SUSHI', 'HNT', 'MATIC ', 'SOL', 'FTM', 'AVAX', 'NEAR', 'LUNA', 'WAVES', 'KAVA', 'XEM', 'XVG', 'DGB', 'QTUM', 'ZRX', 'OMG', 'BAT', 'CVC', 'LOOM', 'MANA', 'SAND', 'ENJ', 'CHZ', 'HBAR  ', 'MATIC', 'SOL', 'FTM', 'AVAX', 'NEAR', 'LUNA', 'WAVES', 'KAVA', 'XEM', 'XVG', 'DGB', 'QTUM', 'ZRX', 'OMG', 'BAT', 'CVC', 'LOOM', 'MANA', 'SAND', 'ENJ', 'CHZ', 'HBAR', 'MATIC', 'SOL', 'FTM', 'AVAX', 'NEAR', 'LUNA', 'WAVES', 'KAVA', 'XEM', 'XVG', 'DGB', 'QTUM', 'ZRX', 'OMG', 'BAT', 'CVC', 'LOOM', 'MANA', 'SAND', 'ENJ', 'CHZ','HBAR', 'XEM', 'XVG', 'DGB', 'QTUM', 'ZRX', 'OMG', 'BAT', 'CVC', 'LOOM', 'MANA', 'SAND', 'ENJ', 'CHZ','HBAR', 'XEM', 'XVG', 'DGB', 'QTUM', 'ZRX', 'OMG', 'BAT', 'CVC', 'LOOM', 'MANA', 'SAND', 'ENJ', 'CHZ','HBAR', 'NGN', 'GHS', 'KES', 'TZS', 'UGX', 'RWF', 'ZMW', 'ZWL', 'MZN', 'SCR', 'SLL', 'SOS', 'TND', 'MAD', 'DZD', 'LYD', 'SDG', 'YER', 'LBP', 'JOD', 'KWD', 'BHD', 'OMR','QAR','AED','SAR','BAM','MKD','ALL','MDL','UAH','BYN','KZT','AZN','GEL','AMD', 'XOF', 'XAF', 'XPF', 'XDR', 'XAU', 'XAG', 'XPT', 'XPD','BTC','ETH','LTC','XMR','DASH','ZEC','DOGE','BNB','SOL','ADA','DOT','TRX','MATIC','AVAX','LINK','XLM','ALGO','ATOM','VET','FIL','ICP'];
 
   const convertCurrency = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
     
     try {
-      const response = await axios.get(`http://localhost:3001/api/convert`, {
-        params: {
-          from: fromCurrency,
-          to: toCurrency,
-          amount: amount,
-        },
-      });
+      const response = await axios.get<ConversionResponse>(
+        'http://localhost:3001/api/convert',
+        {
+          params: {
+            from: fromCurrency,
+            to: toCurrency,
+            amount: amount,
+          },
+        }
+      );
 
       setResult(response.data.result);
       setRate(response.data.rate);
@@ -87,11 +96,11 @@ const CurrencyConverter = () => {
       </form>
 
       {result !== null && rate !== null && (
-        <div className="mt-6 p-4 bg-gray-50 rounded-md">
+        <div className="mt-6 p-4 bg-gray-500 rounded-md">
           <p className="text-lg font-semibold text-center">
             {amount} {fromCurrency} = {result.toFixed(2)} {toCurrency}
           </p>
-          <p className="text-sm text-center text-gray-600 mt-2">
+          <p className="text-sm text-center text-gray-60 mt-2">
             Exchange Rate: 1 {fromCurrency} = {rate.toFixed(4)} {toCurrency}
           </p>
         </div>
